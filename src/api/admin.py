03dd0bca3c09901de85323c25dcb0e3ad1b8d242
Wesley Tam <wesleytam88@gmail.com>
2023-10-19 23:23:06 -0700

    Implement reset function, change shop info

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter, Depends, Request
 from pydantic import BaseModel
 from src.api import auth
+import sqlalchemy
+from src import database as db
 
 router = APIRouter(
     prefix="/admin",
@@ -14,16 +16,38 @@ def reset():
     Reset the game state. Gold goes to 100, all potions are removed from
     inventory, and all barrels are removed from inventory. Carts are all reset.
     """
+    with db.engine.begin() as connection:
+        # Clear all ml, potions, gold, and carts
+        transaction_id = connection.execute(sqlalchemy.text("""
+                                                            TRUNCATE TABLE
+                                                            ledger_transactions, ledger_gold, ledger_ml, ledger_potions, carts, cart_items;
+
+                                                            INSERT INTO ledger_transactions
+                                                            (description)
+                                                            VALUES
+                                                            (:desc)
+                                                            RETURNING id;
+                                                            """),
+                                                            [{"desc": "Add 100 starting gold"}])
+        transaction_id = transaction_id.first()[0]
+
+        # Add 100 starting gold
+        connection.execute(sqlalchemy.text("""
+                                           INSERT INTO ledger_gold
+                                           (transaction_id, change)
+                                           VALUES
+                                           (:transaction_id, :change);
+                                           """),
+                                           [{"transaction_id": transaction_id,
+                                             "change": 100}])
+
     return "OK"
 
 
 @router.get("/shop_info/")
 def get_shop_info():
     """ """
-
-    # TODO: Change me!
     return {
-        "shop_name": "Potion Shop",
-        "shop_owner": "Potion Seller",
+        "shop_name": "The Apothecary",
+        "shop_owner": "Wesley Tam",
     }
-
