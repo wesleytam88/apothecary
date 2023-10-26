8e0f2b04fb853895f4a338708c7f460bc7eb5e52
wesleytam88 <wesleytam88@gmail.com>
2023-10-26 15:41:57 -0700

    Sync with main fork

--- src/api/carts.py
+++ src/api/carts.py
@@ -3,6 +3,7 @@ from pydantic import BaseModel
 from src.api import auth
 import sqlalchemy
 from src import database as db
+from enum import Enum
 
 router = APIRouter(
     prefix="/carts",
@@ -10,6 +11,58 @@ router = APIRouter(
     dependencies=[Depends(auth.get_api_key)],
 )
 
+class search_sort_options(str, Enum):
+    customer_name = "customer_name"
+    item_sku = "item_sku"
+    line_item_total = "line_item_total"
+    timestamp = "timestamp"
+
+class search_sort_order(str, Enum):
+    asc = "asc"
+    desc = "desc"
+
+@router.get("/search/", tags=["search"])
+def search_orders(
+    customer_name: str = "",
+    potion_sku: str = "",
+    search_page: str = "",
+    sort_col: search_sort_options = search_sort_options.timestamp,
+    sort_order: search_sort_order = search_sort_order.desc,
+):
+    """
+    Search for cart line items by customer name and/or potion sku.
+    Customer name and potion sku filter to orders that contain the 
+    string (case insensitive). If the filters aren't provided, no
+    filtering occurs on the respective search term.
+    Search page is a cursor for pagination. The response to this
+    search endpoint will return previous or next if there is a
+    previous or next page of results available. The token passed
+    in that search response can be passed in the next search request
+    as search page to get that page of results.
+    Sort col is which column to sort by and sort order is the direction
+    of the search. They default to searching by timestamp of the order
+    in descending order.
+    The response itself contains a previous and next page token (if
+    such pages exist) and the results as an array of line items. Each
+    line item contains the line item id (must be unique), item sku, 
+    customer name, line item total (in gold), and timestamp of the order.
+    Your results must be paginated, the max results you can return at any
+    time is 5 total line items.
+    """
+
+    return {
+        "previous": "",
+        "next": "",
+        "results": [
+            {
+                "line_item_id": 1,
+                "item_sku": "1 oblivion potion",
+                "customer_name": "Scaramouche",
+                "line_item_total": 50,
+                "timestamp": "2021-01-01T00:00:00Z",
+            }
+        ],
+    }
 
 class NewCart(BaseModel):
     customer: str
@@ -128,20 +181,28 @@ def checkout(cart_id: int, cart_checkout: CartCheckout):
                                                         [{"c_id": cart_id}])
         cart_items = cart_items.all()
 
+        # Update transactions ledger
+        transaction_id = connection.execute(sqlalchemy.text("""
+                                                            INSERT
+                                                            INTO ledger_transactions
+                                                            (description)
+                                                            VALUES (:desc)
+                                                            RETURNING id
+                                                            """),
+                                                            [{"desc": f"Potion(s) sold"}])
+        transaction_id = transaction_id.first()[0]
+
+        connection.execute(sqlalchemy.text("""
+                                           UPDATE carts
+                                           SET transaction_id = :transaction_id
+                                           WHERE id = :cart_id
+                                           """),
+                                           [{"transaction_id": transaction_id,
+                                             "cart_id": cart_id}])
+
         checkout_gold = 0
         checkout_quantity = 0
         for item in cart_items:
-            # Update transactions ledger
-            transaction_id = connection.execute(sqlalchemy.text("""
-                                                                INSERT
-                                                                INTO ledger_transactions
-                                                                (description)
-                                                                VALUES (:desc)
-                                                                RETURNING id
-                                                                """),
-                                                                [{"desc": f"{item.quantity} potion(s) of id {item.potion_id} sold"}])
-            transaction_id = transaction_id.first()[0]
-
             # Update potions ledger
             connection.execute(sqlalchemy.text("""
                                                INSERT
